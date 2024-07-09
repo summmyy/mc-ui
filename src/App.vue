2ce7237094b347329d623e3fb3982cc997dc1d05
Oye <sunmi.oye@bell.ca>
2024-07-09 12:59:59 -0400

    Add show page complete

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import DashboardView from './views/DashboardView.vue'
 // import ShowsView from './views/ShowsView.vue'
 // import GenreView from './views/GenreView.vue'
 // import CountryView from './views/CountryView.vue'
-import AddShowView from './views/AddShowView.vue'
+// import AddShowView from './views/AddShowView.vue'
 </script>
 
 <template>
